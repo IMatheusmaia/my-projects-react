9d5a94400d3893a26daa74cad1372dda2ccec5aa
IMatheusmaia <i.matheuslmaia@gmail.com>
2024-01-18 15:11:52 -0300

    removendo projeto redundante

--- ibge-news-react-ts/src/context/GlobalContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-import { GlobalContextType } from '../types';
-
-const GlobalContext = createContext({
-} as GlobalContextType);
-
-export default GlobalContext;
